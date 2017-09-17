5cf60795b268a2b907454329a80d4470bd87dd48
WP Adictos <WPAdictos@users.noreply.github.com>
2017-09-17 15:13:55 +0200

    Update autoload.php

--- autoload.php
+++ autoload.php
@@ -8,7 +8,5 @@ function __autoload($class_name)
   if(file_exists('./daos/'.$class_name . '.php'))
     require_once ('./daos/'.$class_name . '.php');
   
-    if(file_exists('./util/'.$class_name . '.php'))
-    require_once ('./util/'.$class_name . '.php');
 }
 
