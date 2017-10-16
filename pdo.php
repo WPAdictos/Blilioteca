e05ebf01fa377aa19d0bf280bdab63115af5db54
Jacob <poluxdraco@gmail.com>
2017-10-16 10:45:46 +0200

    SPRINT 6 Parte 2

--- pdo.php
+++ pdo.php
@@ -103,7 +103,7 @@ try {
     $conductor->nombre="Luis";
     $conductor->dni="C237377373";
     $coche= new Coche("HB-3434-3","Seat 11");
-    $coche->setConductor($conductor);
+    
 
     $reg = $conexion->prepare("INSERT INTO Conductores(nombre, dni) VALUES (:nombre, :dni)");
     //Bind de parametros
@@ -112,7 +112,8 @@ try {
     // Ejecutamos
     if ($reg->execute())  echo "Se ha insertado el registro Conductor de forma correcta.<br>";
     $conductor->lastID = $conexion->lastInsertId();  //Guardamos el ultimo id del registro al insertarlo
-   
+    $coche->setConductor($conductor); 
+
     //Insertamos el coche
     $reg = $conexion->prepare("INSERT INTO Coches(marcamodelo, matricula, idconductor) VALUES (:marcamodelo, :matricula, :idconductor)");
     //Bind de parametros
@@ -121,11 +122,29 @@ try {
     $reg->bindParam(':idconductor', $coche->getConductor()->lastID);
     if ($reg->execute())  echo "Se ha insertado el registro Coche de forma correcta.<br>";
 
+    //-----------------------------------------------------------------------------------------------------------------------
+    //modificacion de un regsitro
+
+    //Modificamos el resgitro con ID=7 la matricula
+    $sql = "UPDATE `Coches` SET `matricula`='XXXXXXX' WHERE `id`=7";
+    if ($conexion->exec($sql)){
+        echo "<hr> Se ha actualizado el registro con Id= 7 de forma correcta";
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------------
+    //Borrado de registros usaremos transaciones ya que vamos a borrar dos regsitros
+    $conexion->beginTransaction();
+    $sql = "DELETE FROM Coches  WHERE id=8";
+    echo "<br>Borrando el registro 8 de Coches= " . $conexion->exec($sql);
+    $sql = "DELETE FROM Conductores  WHERE id=14";
+    echo "<br>Borrando el registro 14 de Conductores= " . $conexion->exec($sql);
+    $conexion->commit();
 
 
 
 } catch (PDOException $e){
   echo $e->getMessage();
+  $conexion->rollback();
 }
 $conexion=null;  //Se cierra la conexion
 
